79c2152ea412a070774f56dfa86fbc876f45fd3b
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-16 18:32:37 +0300

    update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Merhaba Node.js")
+var http = require("http")
+
+function requestListener(request, response){
+    response.end()
+}
+
+var server = http.createServer(requestListener)
+server.listen(3000)
+
+console.log("node.js server at port 3000")
