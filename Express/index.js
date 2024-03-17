257cb2779e27d20592f8a57464d93bbbff9df396
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-17 17:01:22 +0300

    middleware

--- Express/index.js
+++ Express/index.js
@@ -2,8 +2,18 @@ const express = require("express")
 
 const app = express()
 
+app.use(function(req, res,next){
+    console.log("middleware 1")
+    next()
+})
+app.use(function(req, res,next){
+    console.log("middleware 2")
+    next()
+})
 app.use(function(req, res){
-    res.end("hello wolwerine")
+    console.log("middleware 3")
+    res.send("<h1>Homepage</h1>")
+    res.end()
 })
 
 app.listen(3000, function(){
