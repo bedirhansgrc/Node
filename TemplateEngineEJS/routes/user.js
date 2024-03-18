730e65fc356808e20485ea7387b7f5d1e1c97ccc
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-18 22:10:11 +0300

    for döngüsü kullanarak dinamik veri ekleme

--- TemplateEngineEJS/routes/user.js
+++ TemplateEngineEJS/routes/user.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
+const data = {
+    title: "Popüler Kurslar",
+    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"]
+}
+
 router.use("/blogs/:blogid", function(req, res,){
     res.render("users/blog-details")
 })
@@ -8,7 +13,7 @@ router.use("/blogs", function(req, res,){
     res.render("users/blogs")
 })
 router.use("/", function(req, res,){
-    res.render("users/index")
+    res.render("users/index", data)
 })
 
 module.exports = router
