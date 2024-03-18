6534841816d0958f9b31b5c849a9833706fd4d4f
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-18 22:44:22 +0300

    if updated

--- TemplateEngineEJS/routes/user.js
+++ TemplateEngineEJS/routes/user.js
@@ -9,19 +9,33 @@ const data = {
             blogid:1,
             baslik:"Komple Uygulamalı Web Geliştirme",
             aciklama:"Web Geliştirme",
-            resim:"1.jpeg"
+            resim:"1.jpeg",
+            anasayfa: true,
+            onay: true
         },
         {
             blogid:2,
             baslik:"Phyton Programlama",
             aciklama:"Phyton Geliştirme",
-            resim:"2.jpeg"
+            resim:"2.jpeg",
+            anasayfa: true,
+            onay: false
         },
         {
             blogid:3,
             baslik:"Javascript Programlama",
             aciklama:"JS Geliştirme",
-            resim:"3.jpeg"
+            resim:"3.jpeg",
+            anasayfa: false,
+            onay: true
+        },
+        {
+            blogid:4,
+            baslik:"Phyton Programlama",
+            aciklama:"Phyton Geliştirme",
+            resim:"2.jpeg",
+            anasayfa: false,
+            onay: true
         }
     ]
 }
