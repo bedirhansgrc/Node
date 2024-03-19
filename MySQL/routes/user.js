a6ef11540b882a013d7c469fe013765d00b3e989
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-19 22:56:49 +0300

    update

--- MySQL/routes/user.js
+++ MySQL/routes/user.js
@@ -3,25 +3,28 @@ const router = express.Router()
 
 const db = require("../data/db")
 
-router.use("/blogs/:blogid", async function(req, res,){
+router.use("/blogs/:blogid", async function (req, res,) {
     const id = req.params.blogid
-    try{
-        const [blog, ] = await db.execute("select * from blog where blogid =?",[id])
-        res.render("users/blog-details", {
-            title: blog[0].baslik,
-            blog: blog[0]
-        })
+    try {
+        const [blog,] = await db.execute("select * from blog where blogid =?", [id])
 
+        if(blog[0]){
+            return res.render("users/blog-details", {
+                title: blog[0].baslik,
+                blog: blog[0]
+            })
+        }
+        res.redirect("/")
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
 })
-router.use("/blogs", async function(req, res,){
+router.use("/blogs", async function (req, res,) {
 
-    try{
-        const [blogs, ] = await db.execute("select * from blog where onay=1")
-        const [categories, ] = await db.execute("select * from category")
+    try {
+        const [blogs,] = await db.execute("select * from blog where onay=1")
+        const [categories,] = await db.execute("select * from category")
         res.render("users/blogs", {
             title: "Tüm Kurslar",
             blogs: blogs,
@@ -29,14 +32,14 @@ router.use("/blogs", async function(req, res,){
         })
     }
 
-    catch(err){
+    catch (err) {
         console.log(err)
     }
 })
-router.use("/", async function(req, res,){
-    try{
-        const [blogs, ] = await db.execute("select * from blog where onay=1 and anasayfa=1")
-        const [categories, ] = await db.execute("select * from category")
+router.use("/", async function (req, res,) {
+    try {
+        const [blogs,] = await db.execute("select * from blog where onay=1 and anasayfa=1")
+        const [categories,] = await db.execute("select * from category")
         res.render("users/index", {
             title: "Popüler Kurslar",
             blogs: blogs,
@@ -44,10 +47,10 @@ router.use("/", async function(req, res,){
         })
     }
 
-    catch(err){
+    catch (err) {
         console.log(err)
     }
-    
+
 })
 
 module.exports = router
