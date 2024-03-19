5ba4c4dea842887efacf299f964e10a19f2cd6ea
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-19 21:01:43 +0300

    update

--- MySQL/index.js
+++ MySQL/index.js
@@ -5,23 +5,6 @@ const app = express()
 
 app.set("view engine","ejs")
 
-const mysql = require("mysql2")
-const config = require("./config")
-
-let connection = mysql.createConnection(config.db)
-
-connection.connect(function(err){
-    if(err){
-        return console.log(err)
-    }
-
-    connection.query("select * from blog", function(err, result){
-        console.log(result)
-    })
-
-    console.log("mysql server bağlantısı yapıldı")
-})
-
 const userRoutes = require("./routes/user")
 const adminRoutes = require("./routes/admin")
 
--- MySQL/routes/user.js
+++ MySQL/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
+const db = require("../data/db")
+
 const data = {
     title: "Popüler Kurslar",
     categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
@@ -44,10 +46,31 @@ router.use("/blogs/:blogid", function(req, res,){
     res.render("users/blog-details")
 })
 router.use("/blogs", function(req, res,){
-    res.render("users/blogs",data)
+    db.execute("select * from blog")
+        .then(result => {
+            res.render("users/index", {
+                title: "Tüm Kurslar",
+                blogs: result[0],
+                categories: data.categories
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
 router.use("/", function(req, res,){
-    res.render("users/index", data)
+    db.execute("select * from blog")
+        .then(result => {
+            res.render("users/index", {
+                title: "Popüler Kurslar",
+                blogs: result[0],
+                categories: data.categories
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    
 })
 
 module.exports = router
