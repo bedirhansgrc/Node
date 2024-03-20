da156ef7473600f8bbb12f99dcabe7a68f6c1640
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-20 18:49:01 +0300

    update

--- YönetimPaneli/routes/admin.js
+++ YönetimPaneli/routes/admin.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const router = express.Router()
 
-router.use("/admin/blog/create", function(req, res,){
-    res.render("admin/blog-create", {
-        title: "add blog"
-    })
+const db = require("../data/db")
+
+router.use("/admin/blog/create",async function(req, res,){
+    try{
+        const [categories, ]= await db.execute("select * from category")
+        res.render("admin/blog-create", {
+            title: "add blog",
+            categories: categories
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
 })
 router.use("/admin/blogs/:blogid", function(req, res,){
     res.render("admin/blog-edit")
