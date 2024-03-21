de1451940011d401bed520afad47d882511c4d00
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-03-21 21:46:55 +0300

    update

--- Denemeler/index.js
+++ Denemeler/index.js
@@ -1,25 +1,10 @@
 const express = require('express');
-const server = express();
-const data = require("./data.js");
+const app = express();
 
-server.get("/", (req, res) => {
+app.use("/", (req, res) => {
     res.end("Hello Wolverine");
 });
 
-server.get("/urunler", (req, res) => {
-    res.status(200).json(data);
-});
-
-server.get("/urunler/:id", (req, res) => {
-    const { id } = req.params;
-    const urun = data.find((urun) => urun.id === parseInt(id));
-    if (urun) {
-        res.status(200).json(urun);
-    } else {
-        res.status(404).send("Aradığınız Ürün Bulunmamaktadır");
-    }
-});
-
-server.listen(3000, () => {
+app.listen(3000, () => {
     console.log(`Server is running on port 3000`);
 });
